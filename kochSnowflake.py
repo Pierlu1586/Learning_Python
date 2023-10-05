f2a1e7a1a2100c562bc4b635862b35abf70de832
Pierluigi Mastroianni <pierluigi-mastroianni@dedagroup.it>
2023-10-05 21:24:43 +0200

    Esercizio sulla Curva di Koch (Frattale)

---
+++ kochSnowflake.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  5 08:31:48 2023
+
+@author: Pierluigi Mastroianni
+"""
+
+# Curva di Koch (Koch snowflake)
+
+import turtle
+
+bob = turtle.Turtle()
+
+def koch(t,l):
+    angolo = 60
+    t.fd(l)
+    t.lt(angolo)            
+    t.fd(l) 
+    t.rt(angolo*2)
+    t.fd(l)
+    t.lt(angolo)
+    t.fd(l)
+
+
+def fioccoDiNeve(f,t,l):
+    if l < 3:
+        t.fd(l*3)
+        return
+    fioccoDiNeve(f, t, l/3)
+    t.lt(60)    
+    fioccoDiNeve(f, t, l/3)        
+    t.rt(120)
+    fioccoDiNeve(f, t, l/3)
+    t.lt(60)
+    fioccoDiNeve(f, t, l/3)
+
+
+fioccoDiNeve(koch, bob, 150)
+
+turtle.mainloop()
