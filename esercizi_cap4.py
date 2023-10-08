4327bdcd038a765434451fb2fdd52f0d0a6638d6
Pierluigi Mastroianni <pierluigi-mastroianni@dedagroup.it>
2023-10-08 14:46:51 +0200

    Ulteriore versione esercizio 4.3.5 + kochSnowflake

--- esercizi_cap4.py
+++ esercizi_cap4.py
@@ -10,6 +10,7 @@ import turtle
 import math
 bob = turtle.Turtle()
 print(bob)
+
 # bob.fd(200)
 # bob.lt(90)
 # bob.fd(200)
@@ -18,7 +19,7 @@ print(bob)
 # Istruzioni per disegnare un quadrato di 200px di lato 
 
 # =============================================================================
-# QUADRATO DISEGNATO CON 'ELENCO DI COMANDI'
+# QUADRATO di 200px  DISEGNATO CON 'ELENCO DI COMANDI'
 # bob.fd(200)
 # bob.lt(90)
 # bob.fd(200)
@@ -26,6 +27,7 @@ print(bob)
 # bob.fd(200)
 # bob.lt(90)
 # bob.fd(200)
+# turtle.mainloop()
 # =============================================================================
 
 # =============================================================================
@@ -64,19 +66,23 @@ print(bob)
 # =============================================================================
 
 
+# =============================================================================
 # ESERCIZIO 4.3.3
-def poligono(t, lunghezza, n):
-      for i in range(n):
-          t.fd(lunghezza)
-          t.lt(360/n)
-        
-poligono(bob, 5, 200)
-
-turtle.mainloop()
+# def poligono(t, lunghezza, n):
+#       for i in range(n):
+#           t.fd(lunghezza)
+#           t.lt(360/n)
+#         
+# poligono(bob, 5, 200)
+# 
+# turtle.mainloop()
+# 
+# =============================================================================
 
 
 
 # =============================================================================
+# ESERCIZIO 4.3.4
 # def poligono(t, lunghezza, n):
 #     """Metodo per costruire dei poligoni regolari"""
 #     for i in range(n):
@@ -85,10 +91,65 @@ turtle.mainloop()
 #           
 # def cerchio(t,r):
 #     circ = 2*math.pi*r
+#     numeroLati = 200
+#     lungLato = circ/numeroLati
+#     poligono(t,lungLato,numeroLati)
 #     
-#     poligono(t,,)
-#     
-#     
-# turtle.mainloop()
+# cerchio(bob, 200)    
 # =============================================================================
+
+
+# =============================================================================
+# ESERCIZIO 4.3.5
+# def poligono(t, lunghezza, n, a):
+#     """Metodo per costruire dei poligoni regolari"""
+#     for i in range(n):
+#         t.fd(lunghezza)
+#         t.lt(a/n)
+#           
+# def arco(t,r,a):
+#      t.lt(90)
+#      circ = 2*math.pi*r
+#      numeroLati = 200
+#      lungLato = (circ/numeroLati)/3
+#      poligono(t,lungLato,numeroLati, a)
+#     
+# arco(bob, 200, 60)
+# =============================================================================
+
+def quadrato(tart, lunghezza):
+    for i in range(4):
+        tart.fd(lunghezza)
+        tart.lt(90)
+
+#quadrato(bob, 50)
+
+def poligono(tart, lunghezza, n, ang):
+    for i in range(n):
+        tart.fd(lunghezza)
+        #dà l'angolo ad ognuno degli nlati da disegnare, questo è ottenuto dividendo l'arco da renderizzare
+        # diviso il numero di lati utilizzati per disegnare l'arco
+        tart.lt(ang/n)  
+
+# poligono(bob, 100, 8)
+
+def cerchio(tart, raggio):
+    circonferenza = 2*math.pi*raggio
+    numeroLati = 200
+    arcoCirc = circonferenza/numeroLati
+    poligono(tart,arcoCirc,numeroLati)
+
+#cerchio(bob, 50)
+
+def arco(tart, raggio, angolo):
+    tart.lt(90) #ruota tartaruga di 90 gradi
+    circonferenza = 2*math.pi*raggio #mi calcola la circonferenza
+    rad = (math.pi*angolo)/180 #converte gli angoli in radianti
+    nlati = int(circonferenza/rad) #determina il numero approssimativo di lati per il poligono da circ e rad 
+    poligono(tart,rad,nlati,angolo)
+    
+arco(bob, 100, 60)    
+
+turtle.mainloop()
+
     
--- kochSnowflake.py
+++ kochSnowflake.py
@@ -12,30 +12,32 @@ import turtle
 
 bob = turtle.Turtle()
 
-def koch(t,l):
-    angolo = 60
-    t.fd(l)
-    t.lt(angolo)            
-    t.fd(l) 
-    t.rt(angolo*2)
-    t.fd(l)
-    t.lt(angolo)
-    t.fd(l)
+# def koch(t,l):
+#     angolo = 60
+#     t.fd(l)
+#     t.lt(angolo)            
+#     t.fd(l) 
+#     t.rt(angolo*2)
+#     t.fd(l)
+#     t.lt(angolo)
+#     t.fd(l)
 
 
-def fioccoDiNeve(f,t,l):
-    if l < 3:
-        t.fd(l*3)
-        return
-    fioccoDiNeve(f, t, l/3)
-    t.lt(60)    
-    fioccoDiNeve(f, t, l/3)        
-    t.rt(120)
-    fioccoDiNeve(f, t, l/3)
-    t.lt(60)
-    fioccoDiNeve(f, t, l/3)
+# def fioccoDiNeve(f,t,l):
+#     if l < 3:
+#         t.fd(l*3)
+#         return
+#     fioccoDiNeve(f, t, l/3)
+#     t.lt(60)    
+#     fioccoDiNeve(f, t, l/3)        
+#     t.rt(120)
+#     fioccoDiNeve(f, t, l/3)
+#     t.lt(60)
+#     fioccoDiNeve(f, t, l/3)
 
 
-fioccoDiNeve(koch, bob, 150)
+# fioccoDiNeve(koch, bob, 150)
+
+def koch()
 
 turtle.mainloop()
